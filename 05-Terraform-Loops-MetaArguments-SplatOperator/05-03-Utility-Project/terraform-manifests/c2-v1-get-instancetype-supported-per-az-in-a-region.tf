abfa18918c379b78283a319bdb877d6943a9d0f9
Srinivas Saini <sainishree@gmail.com>
2022-07-14 15:29:30 +0530

    Added default region and instancetype

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
@@ -1,20 +1,20 @@
-# Datasource
-data "aws_ec2_instance_type_offerings" "my_ins_type1" {
+# Data source
+
+data "aws_ec2_instance_type_offerings" "my_inst_type1" {
   filter {
-    name   = "instance-type"
-    values = ["t3.micro"]
+    name = "instance_type"
+    values = [ "t2.micro" ]
   }
   filter {
-    name   = "location"
-    #values = ["us-east-1a"]
-    values = ["us-east-1e"]
+    name = "location"
+    values = [ap-south-1]
   }
-  location_type = "availability-zone"
+  location_type = "availability_zone"
+  
 }
 
-
-# Output
+# Outputs
 output "output_v1_1" {
-  value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+  value = data.aws.aws_ec2_instance_type_offerings.my_inst_type1.instance_types 
 }
 
