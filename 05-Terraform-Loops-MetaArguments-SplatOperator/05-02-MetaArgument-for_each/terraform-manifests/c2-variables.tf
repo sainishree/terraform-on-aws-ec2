8606cc9b0b22ff5702dad60faf6a0e58e3b7c1d6
Srinivas Saini <sainishree@gmail.com>
2022-07-14 11:44:04 +0530

    Changed default region and instance type to t2micro

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c2-variables.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c2-variables.tf
@@ -3,14 +3,14 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  default = "us-east-1"
+  default = "ap-south-1"
 }
 
 # AWS EC2 Instance Type
 variable "instance_type" {
   description = "EC2 Instnace Type"
   type = string
-  default = "t3.micro"
+  default = "t2.micro"
 }
 
 # AWS EC2 Instance Key Pair
