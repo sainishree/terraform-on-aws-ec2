b50031903ad00a777e1aeeeddac87a69c206964c
Srinivas Saini <sainishree@gmail.com>
2022-07-14 14:34:41 +0530

    path module issue fixed

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c5-ec2instance.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c5-ec2instance.tf
@@ -9,7 +9,7 @@ data "aws_availability_zones" "my_azones" {
 resource "aws_instance" "myec2vm" {
   ami = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
-  user_data = file("${path.module}app1-install.sh")
+  user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
   # Create EC2 in all AZs of a VPC
