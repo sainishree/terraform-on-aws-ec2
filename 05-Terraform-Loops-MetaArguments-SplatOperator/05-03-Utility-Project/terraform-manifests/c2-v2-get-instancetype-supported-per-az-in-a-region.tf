eec4352f4b9cff7ec9ef6add3a492344bf4f8f66
Srinivas Saini <sainishree@gmail.com>
2022-07-15 10:59:31 +0530

    modified version by srinivas saini

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
@@ -1,32 +1,21 @@
 # Check if that respective Instance Type is supported in that Specific Region in list of availability Zones
 # Get the List of Availability Zones in a Particular region where that respective Instance Type is supported
 # Datasource
-data "aws_ec2_instance_type_offerings" "my_ins_type2" {
-  for_each = toset([ "ap-south-1a", "ap-south-1b", "ap-south-1c" ])
+data  "aws_ec2_instance_type_offerings" "my_inst_type2"{
+  for_each = toset(["ap-south-1a","ap-south-1b","ap-south-1c"])
   filter {
-    name   = "instance-type"
+    name = "instance-type"
     values = ["t2.micro"]
   }
   filter {
-    name   = "location"
+    name = "locatioin"
     values = [each.key]
   }
   location_type = "availability-zone"
 }
 
-
-#Output-1
-# Important Note: Once for_each is set, its attributes must be accessed on specific instances
+# Outputs
 output "output_v2_1" {
-  #value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
-  value = toset([for t in data.aws_ec2_instance_type_offerings.my_ins_type2: t.instance_types])
-}
+  value = toset([for i in data.aws_ec2_instance_type_offerings.my_inst_type2: i.instance_types])
 
-#Output-2
-# Create a Map with Key as Availability Zone and value as Instance Type supported
-output "output_v2_2" {
-  value = {
-    for az, details in data.aws_ec2_instance_type_offerings.my_ins_type2: az => details.instance_types
-  }
 }
-
